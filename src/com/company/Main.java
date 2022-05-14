71ee16815106e73396aa238f6c3573f94155722e
Mateusz <MATEUSZ24.PS@GMAIL.COM>
2022-05-14 20:54:18 +0200

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.company.mylib.Account;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Account acc = new Account();
+        acc.setName("piotr Gołabek");
+        System.out.println(acc.getName());
     }
 }
---
+++ src/com/company/mylib/Account.java
@@ -0,0 +1,18 @@
+package com.company.mylib;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Account {
+    private String name;
+    public void setName(String name) {
+        String[] tokens = StringUtils.split(name);
+        for(int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        this.name = StringUtils.join(tokens,' ');
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
